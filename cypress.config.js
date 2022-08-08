3ab78eada4276b802897b64c56ef88b39b812146
Ryan Wilke <ryanwilke@gmail.com>
2022-08-08 21:01:49 +0200

    Testing the connection to the dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "czspkg",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
